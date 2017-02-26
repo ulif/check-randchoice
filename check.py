c03f05acdb5de124aa08055f39dcb35891c39b04
ulif <uli@gnufix.de>
2017-02-26 02:03:09 +0100

    Eliminate `myrandom` var.

--- check.py
+++ check.py
@@ -29,17 +29,16 @@ class FakeURandom(object):
 
 
 fake_random = FakeURandom()
-myrandom = random.SystemRandom()
 random._urandom = fake_random.urandom
 
 
-check1 = myrandom.random()
+check1 = random.SystemRandom().random()
 print("check1 %s" % check1)
 assert check1 == 0.0
 
 
 fake_random.bits = ["\xff", "\xff"]
-check2 = myrandom.random()
+check2 = random.SystemRandom().random()
 print("check2 %s" % check2)
 assert str(check2) == "0.999984741211"
 
@@ -47,32 +46,32 @@ assert str(check2) == "0.999984741211"
 num1_bits = num_to_urandom_bits(2**56-1)
 print(num1_bits)
 fake_random.bits = num1_bits
-check3 = myrandom.random()
+check3 = random.SystemRandom().random()
 print("check3 %s" % check3)
 assert str(check3) == "1.0"
 
 fake_random.bits = num_to_urandom_bits(2**56 - 1)
-check4 = myrandom.choice([1, 2, 3])
+check4 = random.SystemRandom().choice([1, 2, 3])
 print("check4 %s" % check4)
 assert check4 == 3
 
 fake_random.bits = num_to_urandom_bits(2**55 - 1)
-check5 = myrandom.choice([1, 2, 3])
+check5 = random.SystemRandom().choice([1, 2, 3])
 print("check5 %s" % check5)
 assert check5 == 2
 
 fake_random.bits = num_to_urandom_bits(0)
-check6 = myrandom.choice([1, 2, 3])
+check6 = random.SystemRandom().choice([1, 2, 3])
 print("check6 %s" % check6)
 assert check6 == 1
 
 fake_random.bits = num_to_urandom_bits(24019198012642647)
-check7 = myrandom.choice([1, 2, 3])
+check7 = random.SystemRandom().choice([1, 2, 3])
 print("check7 %s" % check7)
 assert check7 == 1
 
 fake_random.bits = num_to_urandom_bits(24019198012642648)
-check8 = myrandom.choice([1, 2, 3])
+check8 = random.SystemRandom().choice([1, 2, 3])
 print("check8 %s" % check8)
 assert check8 == 2
 
@@ -82,7 +81,7 @@ def part(t, t0, t1, r_min=0, r_max=2**56):
         return r_min, r_max
     r_new = r_min + ((r_max - r_min) / 2)
     fake_random.bits = num_to_urandom_bits(r_new)
-    tn = myrandom.choice([1, 2, 3])
+    tn = random.SystemRandom().choice([1, 2, 3])
     if tn > t0 and tn > t:
         return part(t, t0, tn, r_min, r_new)
     else:
