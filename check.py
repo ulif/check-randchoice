562678b157931276c04c9a823b8f068ac5d8520f
ulif <uli@gnufix.de>
2017-03-07 17:39:36 +0100

    Remove some flakes.

--- check.py
+++ check.py
@@ -54,20 +54,17 @@ check2 = random.SystemRandom().random()
 print("check2 %s" % check2)
 assert str(check2) == "0.999984741211"
 
-
-num1_bits = num_to_bytes(2**56-1)
-print(num1_bits)
-fake_random.bytes_list = num1_bits
+fake_random.bytes_list = num_to_bytes(2 ** 56 - 1)
 check3 = random.SystemRandom().random()
 print("check3 %s" % check3)
 assert str(check3) == "1.0"
 
-fake_random.bytes_list = num_to_bytes(2**56 - 1)
+fake_random.bytes_list = num_to_bytes(2 ** 56 - 1)
 check4 = random.SystemRandom().choice([1, 2, 3])
 print("check4 %s" % check4)
 assert check4 == 3
 
-fake_random.bytes_list = num_to_bytes(2**55 - 1)
+fake_random.bytes_list = num_to_bytes(2 ** 55 - 1)
 check5 = random.SystemRandom().choice([1, 2, 3])
 print("check5 %s" % check5)
 assert check5 == 2
@@ -88,7 +85,7 @@ print("check8 %s" % check8)
 assert check8 == 2
 
 
-def part(t, t0, t1, r_min=0, r_max=2**56):
+def part(t, t0, t1, r_min=0, r_max=2 ** 56):
     if r_max == r_min + 1:
         return r_min, r_max
     r_new = r_min + ((r_max - r_min) / 2)
@@ -99,8 +96,9 @@ def part(t, t0, t1, r_min=0, r_max=2**56):
     else:
         return part(t, tn, t1, r_new, r_max)
 
-print(part(1, 1, 3, 0, 2**56))
-print(part(2, 1, 3, 0, 2**56))
+
+print(part(1, 1, 3, 0, 2 ** 56))
+print(part(2, 1, 3, 0, 2 ** 56))
 
 print("range '1': ", 24019198012642647 + 1)
 print("range '2': ", 48038396025285287 - 24019198012642647)
